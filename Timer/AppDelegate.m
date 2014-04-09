5d44015e4a92c11c8529b0d2adf7e4a1c871e43f
asakasinsky <asakasinsky@gmail.com>
2014-04-09 21:47:02 +0400

    Initial Commit

---
+++ Timer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Timer
+//
+//  Created by asakasinsky on 09.04.14.
+//  Copyright (c) 2014 aaaxe. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Timer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Timer
+//
+//  Created by asakasinsky on 09.04.14.
+//  Copyright (c) 2014 aaaxe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
